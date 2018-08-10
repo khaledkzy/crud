8c704e5b876bdcda09171859cf17fb827ef2185a
khaledkzy <khaledkzy@gmail.com>
2018-08-10 12:14:22 +0100

    feat: seed the table with sample data

---
+++ server/seeds/todo.js
@@ -0,0 +1,20 @@
+
+exports.seed = function (knex, Promise) {
+  return knex('todo').del() // remove all the rows in the todo table
+    .then(function () {
+      const todos = [{
+        title: 'Build a CRUD App',
+        priority: 1,
+        date: new Date()
+      }, {
+        title: 'Buy a table cover',
+        priority: 3,
+        date: new Date()
+      }, {
+        title: 'Go to the Lidl',
+        priority: 2,
+        date: new Date()
+      }]
+  return knex('todo').insert(todos); // now insert them
+});
+};
