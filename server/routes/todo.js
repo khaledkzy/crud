f08c02f9bb3356b0d156cf567ee67e048133ddbc
khaledkzy <khaledkzy@gmail.com>
2018-08-10 14:46:56 +0100

    feat: show a from with /todo/new

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,15 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const knex = require('../db/knex')
 
 /* This router is mounted at http://localhost:3000/todo */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res) => {
     knex('todo')
         .select()
         .then(todos => {
             res.render('all', { todos: todos });
         })
+});
 
+router.get('/new', (req, res, next) => {
+    res.render('new');
 });
 
 module.exports = router;
